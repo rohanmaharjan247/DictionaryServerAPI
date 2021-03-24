1364415b6b3a20d5d4d7a92499f338919b2a1310
Rohan Maharjan <smdnoobs24@gmail.com>
2021-03-24 14:45:13 +0545

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 8083;
 const dictionaryApi = "https://od-api.oxforddictionaries.com/api/v2";
+
+app.use(cors());
 
 app.get('/', function(req, res){
   res.send("Welcome to Dictionary API")
@@ -17,13 +21,11 @@ app.get('/dictionarysearch/:language/:wordId', async (req, res) => {
   const fileRead = fs.readFileSync('./config.json', 'utf-8');
   const auths = JSON.parse(fileRead);
   const app_id = auths.app_id;
-  console.log(auths, 'auths');
-  console.log(app_id, 'appid');
   const app_key =  auths.app_key;
-  console.log(app_key, 'appkey');
+
 
   const apiUrl = `${dictionaryApi}/entries/${language}/${wordId.toLowerCase()}`;
-  console.log(apiUrl);
+
   try{
     const response = await axios.get(apiUrl, {
       headers:  {
